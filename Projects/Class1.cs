efca57bed1f02dcce4e754f204b827a3156677eb
samibhatti <abdulsami.bhatti@gmail.com>
2016-12-16 03:15:33 +0100

    First commit

---
+++ Projects/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Chapter1
+{
+    public static class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; int < 10; i++)
+                Console.WriteLine("ThreadProc: {0}", i);
+            Thread.Sleep(0);
+        }
+        
+	}
+
+    public static void Main()
+    {
+        Thread t = new Thread(new ThreadStart(ThreadMethod));
+        t.Start();
+
+        for (int i = 0; i < 4; i++)
+        {
+            Console.WriteLine("Main thread: Do some work.");
+            Thread.Sleep(0);
+        }
+
+        t.Join();
+    }
+}
---
+++ Projects/SimpleRESTserver/SimpleRESTserver/Controllers/PersonalController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleRESTserver.Controllers
+{
+    public class PersonalController : ApiController
+    {
+        // GET: api/Personal
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "person4", "person3" };
+        }
+
+        // GET: api/Personal/5
+        public string Get(int id)
+        {
+            return "some person";
+        }
+
+        // POST: api/Personal
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Personal/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Personal/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
