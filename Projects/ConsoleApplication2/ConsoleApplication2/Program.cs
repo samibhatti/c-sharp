c33ddf42bb6195af82f2b879ffbde7518600a668
samibhatti <abdulsami.bhatti@gmail.com>
2016-12-16 04:58:04 +0100

    github

---
+++ Projects/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication2
+{
+    public static class Program
+    {
+        public static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("ThreadProc: {0}", i);
+                Thread.Sleep(10000);
+            }
+        }
+        public static void Main()
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.IsBackground = false;
+            t.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("work work work");
+            }
+            //Console.ReadKey();
+
+        }
+    }
+}
